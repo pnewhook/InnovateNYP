2432962bd1c191c19db3ecb3b860bcff7631b702
Peter Newhook <peter.newhook@gmail.com>
2014-03-15 11:34:31 -0400

    trying to make it work under https

--- InnovateNYP/App_Start/BundleConfig.cs
+++ InnovateNYP/App_Start/BundleConfig.cs
@@ -23,9 +23,11 @@ namespace InnovateNYP
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
+            bundles.Add(new StyleBundle("~/Content/style").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/sb-admin.css",
+                      "~/font-awesome/css/font-awesome.min.css",
+                      "~/Content/morris-0.4.3.min.css"));
         }
     }
 }
