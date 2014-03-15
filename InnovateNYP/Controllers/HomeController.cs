c0e29df4a49a967ff6ff8e6e93efb01d492b7c99
Peter Newhook <peter.newhook@gmail.com>
2014-03-15 11:18:29 -0400

    custom pages

--- InnovateNYP/Controllers/HomeController.cs
+++ InnovateNYP/Controllers/HomeController.cs
@@ -15,15 +15,22 @@ namespace InnovateNYP.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
 
+            return View();
+        }
+
+        public ActionResult Medication()
+        {
+            return View();
+        }
+
+        public ActionResult Insight()
+        {
             return View();
         }
     }
